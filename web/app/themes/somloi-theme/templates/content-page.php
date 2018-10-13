610ed00c5288c9788ff58ea33724db356f3ecde1
lukacsaron <aron.lukacs@flashbirdcreative.com>
2018-10-13 16:08:19 +0200

    start shop design

--- web/app/themes/somloi-theme/templates/content-page.php
+++ web/app/themes/somloi-theme/templates/content-page.php
@@ -1,2 +1,7 @@
 <?php the_content(); ?>
+<span class="ekho"><?php echo LSCF_PLUGIN_PATH; ?></span><br>
+<span class="ekho"><?php echo LSCF_PLUGIN_URL; ?></span><br>
+<span class="ekho"><?php echo LSCF_MAIN_PATH  ; ?></span><br>
+<span class="ekho"><?php echo ABSPATH  ; ?></span><br>
+
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
---
+++ web/app/themes/somloi-theme/templates/shop-template.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Shop Template
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+
+   
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
